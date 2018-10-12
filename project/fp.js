e02ff475462c7b9da1a3fc889e778e6f67013b27
2ZayaZ <ZayaZzz@gmail.com>
2018-10-12 16:56:36 +0300

    2nd commit

---
+++ project/fp.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("select-item-1 span").hide();
+  // $("select-item-1 onClick").click(function() {
+  //   $(this)
+  //     .parent()
+  //     .next()
+  //     .slideToggle();
+  // });
+});
